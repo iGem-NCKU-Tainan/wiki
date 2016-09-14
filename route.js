fac3aa8707a1af1e02c4eb8ba375d88a22915159
jingfei <jingfei955047@gmail.com>
2016-09-14 16:40:40 +0800

    Fix title disappear bug.

--- route.js
+++ route.js
@@ -3,55 +3,67 @@ var url = require('url');
 
 route = [
   {
-    "title": "iGEM NCKU",
-    "url": "",
-    "img": "",
-    "description": "",
+		data: {
+	    "title": "iGEM NCKU",
+    	"url": "",
+			"img": "",
+    	"description": "",
+		},
     "partials": './partials.js',
     "layout":  "./view/index.hbs",
     "filename": "./docs/index.html"
   },
   {
-    "title": "Project - iGEM NCKU",
-    "url": "",
-    "img": "",
-    "description": "",
+    data: {
+			"title": "Project - iGEM NCKU",
+  	  "url": "",
+ 	   	"img": "",
+  	  "description": "",
+		},
     "partials": './partials.js',
     "layout":  "./view/Project/index.hbs",
     "filename": "./docs/Project/index.html"
   },
   {
-    "title": "Wet lab - iGEM NCKU",
-    "url": "",
-    "img": "",
-    "description": "",
+		data: {
+  	  "title": "Wet lab - iGEM NCKU",
+	    "url": "",
+  	  "img": "",
+  	  "description": "",
+		},
     "partials": './partials.js',
     "layout":  "./view/Wetlab/index.hbs",
     "filename": "./docs/Wetlab/index.html"
   },
   {
-    "title": "Dry lab - iGEM NCKU",
-    "url": "",
-    "img": "",
-    "description": "",
+		data: {
+	    "title": "Dry lab - iGEM NCKU",
+  	  "url": "",
+ 	  	"img": "",
+  	  "description": "",
+		},
     "partials": './partials.js',
     "layout":  "./view/Drylab/index.hbs",
     "filename": "./docs/Drylab/index.html"
   },
   {
-    "title": "Human Practices - iGEM NCKU",
-    "url": "",
-    "img": "",
-    "description": "",
+		data: {
+	    "title": "Human Practices - iGEM NCKU",
+  	  "url": "",
+    	"img": "",
+    	"description": "",
+		},
     "partials": './partials.js',
     "layout":  "./view/Human_Practices/index.hbs",
     "filename": "./docs/Human_Practices/index.html"
   },
   {
-    "title": "Team - iGEM NCKU",
-    "url": "",
-    "img": "",
-    "description": "",
+		data: {
+	    "title": "Team - iGEM NCKU",
+  	  "url": "",
+    	"img": "",
+    	"description": "",
+		},
     "partials": './partials.js',
     "layout":  "./view/Team/index.hbs",
     "filename": "./docs/Team/index.html"
