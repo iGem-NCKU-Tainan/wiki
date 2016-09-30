3171eb87621d20a8b8b4547e7c0271e689eb09a5
chihsuan <chihsuan.tw@gmail.com>
2016-09-30 19:05:53 +0800

    update menu
    change style, move submenu below the main menu

--- js/T--NCKU_Tainan--common.js
+++ js/T--NCKU_Tainan--common.js
@@ -5,12 +5,15 @@
 
 $(window).on("load", function(){
 	var container = document.getElementsByClassName("content")[0];
+	if (container === undefined) return;
+
 	/* content's height */
-	var col1 = container.getElementsByClassName("col-md-9")[0];
-	var col2 = container.getElementsByClassName("col-md-3")[0];
-	var h1 = col1.offsetHeight, h2 = col2.offsetHeight;
-	if(h2>h1) col1.style.height = h2;
-	else col2.style.height = h1;
+	var col1 = container.getElementsByClassName("col-md-9");
+	var col2 = container.getElementsByClassName("col-md-3");
+
+	if (col1 !== undefined && col2 !== undefined) {
+	  updateColsHeight(col1, col2);
+  }
 
 	/* add content page's comment */
 	var content = container.getElementsByClassName("title-line");
@@ -24,3 +27,13 @@ $(window).on("load", function(){
 		item2.style.width = "90%";
 	}
 });
+
+function updateColsHeight(col1, col2) {
+	var h1 = col1.offsetHeight, h2 = col2.offsetHeight;
+	if (h2 > h1) {
+	  col1.style.height = h2;
+  }
+	else {
+	  col2.style.height = h1;
+  }
+}
