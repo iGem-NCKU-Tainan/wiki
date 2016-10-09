558fb6190fcc8321167a3cdf9b4924b266d32bd6
jingfei <jingfei955047@gmail.com>
2016-10-09 15:09:45 +0800

    Add safety's images.

--- dev_routes/route-judging.js
+++ dev_routes/route-judging.js
@@ -18,6 +18,11 @@ module.exports = [
       img: "",
       description: "",
       css: "/wiki/css/T--NCKU_Tainan--Layout.css",
+			image0: "/wiki/images/judging/T--NCKU_Tainan--safety0.jpg",
+			image1: "/wiki/images/judging/T--NCKU_Tainan--safety1.jpg",
+			image2: "/wiki/images/judging/T--NCKU_Tainan--safety2.jpg",
+			image3: "/wiki/images/judging/T--NCKU_Tainan--safety3.jpg",
+			image4: "/wiki/images/judging/T--NCKU_Tainan--safety4.jpg",
       sample2: "/wiki/images/T--NCKU_Tainan--sample2.jpg"
     },
     layout:  "./view/judging/safety.hbs",
