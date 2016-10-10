3a1d55b8e984fd2a3788dc46db07468e6e18d366
xr2439 <xr2439@gmail.com>
2016-10-10 23:44:41 +0800

    Fix Mathjax issue

--- js/T--NCKU_Tainan--MathjaxConfigIgem.js
+++ js/T--NCKU_Tainan--MathjaxConfigIgem.js
@@ -1,7 +1,8 @@
 if (window.MathJax)
 MathJax.Hub.Config({
   tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]},
-  "HTML-CSS": {scale: 120},
-  CommonHTML: {scale: 120},
-  SVG: {scale:120}
+  displayMath: [ ['$$','$$']],
+  "HTML-CSS": {scale: 150},
+  CommonHTML: {scale: 150},
+  SVG: {scale:150}
 });
--- js/T--NCKU_Tainan--MathjaxConfigLocal.js
+++ js/T--NCKU_Tainan--MathjaxConfigLocal.js
@@ -1,6 +1,7 @@
 if (window.MathJax)
 MathJax.Hub.Config({
   tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]},
+  displayMath: [ ['$$','$$']],
   "HTML-CSS": {scale: 120},
   CommonHTML: {scale: 120},
   SVG: {scale:120}
