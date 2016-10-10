80fd8852212d90d727cebb6fef00bfb2f32af681
xr2439 <xr2439@gmail.com>
2016-10-10 23:53:16 +0800

    Fix Mathjax issue & Deploy(Update to igem's website)

--- js/T--NCKU_Tainan--MathjaxConfigLocal.js
+++ js/T--NCKU_Tainan--MathjaxConfigLocal.js
@@ -2,7 +2,7 @@ if (window.MathJax)
 MathJax.Hub.Config({
   tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]},
   displayMath: [ ['$$','$$']],
-  "HTML-CSS": {scale: 120},
-  CommonHTML: {scale: 120},
-  SVG: {scale:120}
+  "HTML-CSS": {scale: 105},
+  CommonHTML: {scale: 105},
+  SVG: {scale:105}
 });
--- product/js/T--NCKU_Tainan--MathjaxConfigLocal.js
+++ product/js/T--NCKU_Tainan--MathjaxConfigLocal.js
@@ -1,1 +1,1 @@
-window.MathJax&&MathJax.Hub.Config({tex2jax:{inlineMath:[["$","$"],["\\(","\\)"]]},displayMath:[["$$","$$"]],"HTML-CSS":{scale:120},CommonHTML:{scale:120},SVG:{scale:120}});
+window.MathJax&&MathJax.Hub.Config({tex2jax:{inlineMath:[["$","$"],["\\(","\\)"]]},displayMath:[["$$","$$"]],"HTML-CSS":{scale:105},CommonHTML:{scale:105},SVG:{scale:105}});
