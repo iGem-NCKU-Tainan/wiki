52ca0aeb0da3ec9132536a6f969ae3fc930e4b0f
xr2439 <xr2439@gmail.com>
2016-10-10 23:46:22 +0800

    Deploy

--- docs/js/T--NCKU_Tainan--MathjaxConfigIgem.js
+++ docs/js/T--NCKU_Tainan--MathjaxConfigIgem.js
@@ -1,1 +1,1 @@
-window.MathJax&&MathJax.Hub.Config({tex2jax:{inlineMath:[["$","$"],["\\(","\\)"]]},"HTML-CSS":{scale:120},CommonHTML:{scale:120},SVG:{scale:120}});
+window.MathJax&&MathJax.Hub.Config({tex2jax:{inlineMath:[["$","$"],["\\(","\\)"]]},displayMath:[["$$","$$"]],"HTML-CSS":{scale:150},CommonHTML:{scale:150},SVG:{scale:150}});
